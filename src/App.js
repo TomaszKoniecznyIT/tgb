057e9c369ce6829455457cee34b269b3dd8bd2fe
TomaszKoniecznyIT <tkonieczny.it@wp.pl>
2023-11-09 14:57:40 +0000

    adding ErrorPage

--- src/App.js
+++ src/App.js
@@ -1,13 +1,16 @@
 import { RouterProvider, createBrowserRouter } from "react-router-dom";
+
 import HomePage from "./pages/Home";
 import ManagerPage from "./pages/Manager";
 import ShopPage from "./pages/Shop";
 import RootLayout from "./pages/Root";
+import ErrorPage from "./pages/Error";
 
 const router = createBrowserRouter([
   {
     path: "/",
     element: <RootLayout />,
+    errorElement: <ErrorPage />,
     children: [
       {
         path: "/",
