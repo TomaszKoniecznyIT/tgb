5027bb8a8086d4a2e000d70c9b07e254fe37bfca
TomaszKoniecznyIT <tkonieczny.it@wp.pl>
2023-11-09 14:05:05 +0000

    adding ManagerPage

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import { RouterProvider, createBrowserRouter } from "react-router-dom";
 import HomePage from "./pages/Home";
+import ManagerPage from "./pages/Manager";
 
 const router = createBrowserRouter([
   {
     path: "/",
     element: <HomePage />,
   },
+  { path: "manager", element: <ManagerPage /> },
 ]);
 
 function App() {
---
+++ src/pages/Manager.js
@@ -0,0 +1,5 @@
+function ManagerPage() {
+  return <h1>Menager Page</h1>;
+}
+
+export default ManagerPage;
