2f4e125fae95c3a4203d825edd665829942a473b
TomaszKoniecznyIT <tkonieczny.it@wp.pl>
2023-11-09 14:07:48 +0000

    adding ShopPage

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import { RouterProvider, createBrowserRouter } from "react-router-dom";
 import HomePage from "./pages/Home";
 import ManagerPage from "./pages/Manager";
+import ShopPage from "./pages/Shop";
 
 const router = createBrowserRouter([
   {
@@ -8,6 +9,7 @@ const router = createBrowserRouter([
     element: <HomePage />,
   },
   { path: "manager", element: <ManagerPage /> },
+  { path: "shop", element: <ShopPage /> },
 ]);
 
 function App() {
