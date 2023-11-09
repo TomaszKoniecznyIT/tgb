0945d95bbce108a134b93d1ac8edd8095254f2ab
TomaszKoniecznyIT <tkonieczny.it@wp.pl>
2023-11-09 15:05:36 +0000

    adding classes inActive

--- src/components/MainNavigation.js
+++ src/components/MainNavigation.js
@@ -8,13 +8,34 @@ function MainNavigation() {
       <nav>
         <ul className={classes.list}>
           <li>
-            <NavLink to="/">Home</NavLink>
+            <NavLink
+              to="/"
+              className={({ isActive }) =>
+                isActive ? classes.active : undefined
+              }
+            >
+              Home
+            </NavLink>
           </li>
           <li>
-            <NavLink to="/manager">Manager</NavLink>
+            <NavLink
+              to="/manager"
+              className={({ isActive }) =>
+                isActive ? classes.active : undefined
+              }
+            >
+              Manager
+            </NavLink>
           </li>
           <li>
-            <NavLink to="/shop">Shop</NavLink>
+            <NavLink
+              to="/shop"
+              className={({ isActive }) =>
+                isActive ? classes.active : undefined
+              }
+            >
+              Shop
+            </NavLink>
           </li>
         </ul>
       </nav>
