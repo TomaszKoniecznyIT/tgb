2e7955060dc901fd7ff7688d1bfeba007eedac98
TomaszKoniecznyIT <tkonieczny.it@wp.pl>
2023-11-23 08:11:12 +0000

    adding LoginForm

---
+++ src/components/LoginForm.js
@@ -0,0 +1,38 @@
+import { useMutation } from "@tanstack/react-query";
+
+import { createNewUser } from "../util/http";
+import classes from "./NewUserForm.module.css";
+
+function LoginForm() {
+  const { mutate } = useMutation({
+    mutationFn: createNewUser,
+  });
+
+  function handleSubmit(event) {
+    event.preventDefault();
+    const formData = new FormData(event.target);
+    const userData = Object.fromEntries(formData);
+    mutate(userData);
+  }
+
+  return (
+    <>
+      <form className={classes.form} onSubmit={handleSubmit}>
+        <h1>Login</h1>
+        <p>
+          <label htmlFor="email">Email</label>
+          <input id="email" type="email" name="email" required />
+        </p>
+        <p>
+          <label htmlFor="password">Password</label>
+          <input id="password" type="password" name="password" required />
+        </p>
+        <div className={classes.actions}>
+          <button type="submit">Login</button>
+        </div>
+      </form>
+    </>
+  );
+}
+
+export default LoginForm;
