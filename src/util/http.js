3942e14437aa95eca40c79e22f795671a370cde6
TomaszKoniecznyIT <tkonieczny.it@wp.pl>
2023-12-01 22:06:48 +0000

    saving the is_manager variable in localStorage

--- src/util/http.js
+++ src/util/http.js
@@ -1,4 +1,4 @@
-import { redirect } from "react-router-dom";
+import { jwtDecode } from "jwt-decode";
 
 export async function createNewUser(userData) {
   const response = await fetch("http://127.0.0.1:5000/users/signup", {
@@ -30,8 +30,11 @@ export async function login(userData) {
   const resData = await response.json();
 
   const token = resData.token;
-  console.log(resData);
   localStorage.setItem("token", token);
+  console.log(token);
+  const is_manager = jwtDecode(token).is_manager;
+  localStorage.setItem("is_manager", is_manager);
+  console.log(is_manager);
 
   return resData.message;
 }
