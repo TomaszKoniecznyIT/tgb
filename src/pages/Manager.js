02ab04909895f8167e33e5effa0f82d2aac95956
TomaszKoniecznyIT <tkonieczny.it@wp.pl>
2023-12-03 08:37:20 +0000

    adding option names

--- src/pages/Manager.js
+++ src/pages/Manager.js
@@ -1,5 +1,16 @@
 function ManagerPage() {
-  return <h1>Menager Page</h1>;
+  return (
+    <>
+      <div>
+        <h1>Menager Page</h1>;
+      </div>
+      <div>
+        <h3>Add a new shop</h3>
+        <h3>Add a monthly target for shop</h3>
+        <h3>Report</h3>
+      </div>
+    </>
+  );
 }
 
 export default ManagerPage;
--- src/pages/Shop.js
+++ src/pages/Shop.js
@@ -1,5 +1,15 @@
 function ShopPage() {
-  return <h1>Shop Page</h1>;
+  return (
+    <>
+      <div>
+        <h1>Shop Page</h1>;
+      </div>
+      <div>
+        <h3>Add daily sales</h3>
+        <h3>Report</h3>
+      </div>
+    </>
+  );
 }
 
 export default ShopPage;
