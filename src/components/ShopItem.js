9d09617f88a58a6e925666a92b3539ae1f2a9b5c
TomaszKoniecznyIT <tkonieczny.it@wp.pl>
2023-12-06 19:31:25 +0000

    adding buttons depending is_manager

--- src/components/ShopItem.js
+++ src/components/ShopItem.js
@@ -1,10 +1,15 @@
 function ShopItem({ shop }) {
+  const is_manager =
+    localStorage.getItem("is_manager") === "true" ? true : false;
+
   return (
     <div>
       <h1>SHOP DETAILS</h1>
       <h2>{shop.name}</h2>
       <p>{shop.email}</p>
       <p>{shop.shop_code}</p>
+      {is_manager && <button>Add Target</button>}
+      {!is_manager && <button>Add Sale</button>}
     </div>
   );
 }
