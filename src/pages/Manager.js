ac05b35db5bc7e8e38d8c393d0798448cd1506c5
TomaszKoniecznyIT <tkonieczny.it@wp.pl>
2023-12-28 08:57:11 +0000

    blocking access to the manager page for stores

--- src/pages/Manager.js
+++ src/pages/Manager.js
@@ -1,21 +1,31 @@
 import { Link } from "react-router-dom";
 
 function ManagerPage() {
-  return (
-    <>
-      <div>
-        <h1>Menager Page</h1>
-      </div>
-      <div>
-        <p>
-          <Link to="new_shop">Add a new shop</Link>
-        </p>
-        <p>
-          <Link to="shops">Show all shops.</Link>
-        </p>
-      </div>
-    </>
-  );
+  const is_manager =
+    localStorage.getItem("is_manager") === "true" ? true : false;
+
+  let content;
+
+  if (is_manager) {
+    content = (
+      <>
+        <div>
+          <h1>Menager Page</h1>
+        </div>
+        <div>
+          <p>
+            <Link to="new_shop">Add a new shop</Link>
+          </p>
+          <p>
+            <Link to="shops">Show all shops.</Link>
+          </p>
+        </div>
+      </>
+    );
+  } else {
+    content = <div>The shop does not have access to this page.</div>;
+  }
+  return content;
 }
 
 export default ManagerPage;
