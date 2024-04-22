8fac0fc2816a1976ac9055afaa99b03d4c4fccb3
TomaszKoniecznyIT <tkonieczny.it@wp.pl>
2024-04-22 12:53:40 +0100

    report data style 2

--- src/components/Report.js
+++ src/components/Report.js
@@ -43,10 +43,14 @@ function Report({ report }) {
           ))}
         </ol>
       </div>
-      <h1>Total sales for the specified period.</h1>
-      <div>{totalSales.toFixed(2)}</div>
-      <h1>Average daily sales</h1>
-      <div>{avgSales.toFixed(2)}</div>
+      <div className={classes.item}>
+        <h1>Total sales for the specified period.</h1>
+        <div>{totalSales.toFixed(2)}</div>
+      </div>
+      <div className={classes.item}>
+        <h1>Average daily sales</h1>
+        <div>{avgSales.toFixed(2)}</div>
+      </div>
       <div>
         <BarChart chartData={reportData} />
       </div>
